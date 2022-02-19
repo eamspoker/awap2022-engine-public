42223c7509802ee1bc87923a287bd38a58d9e6ec
Ellia Yang <elyang1221@gmail.com>
2022-02-19 14:08:47 -0500

    BFS

--- bots/template_bot.py
+++ bots/template_bot.py
@@ -16,6 +16,44 @@ class MyPlayer(Player):
 
 
     def play_turn(self, turn_num, map, player_info):
+       priorityQueue = self.BFS(map, 0,0, [], [], [], player_info)
+       return
 
 
-        return
+    def getChild(i, j, dir, map):
+        drow, dcol = dir
+        return map[i+drow][j+dcol]
+
+    def BFS(self, map, i, j, visited, priorityQueue, queue, player_info):
+        queue.append(map[i][j])
+        visited.append(map[i][j])
+
+        dirs = [(1,0), (-1, 0), (0,1), (0, -1)]
+
+        while queue != []:
+            st1 = queue.pop(0).structure
+            if st1 is not None & st1.team == player_info.team:
+                for dir in dirs:
+                    newNode = self.getChild(i, j, dir, map)
+                    if newNode not in visited:
+                        queue.append(newNode)
+                        visited.append(newNode)
+                        st = newNode.structure
+                        if st is None:
+                            priorityQueue.append(newNode)
+            else:
+                for dir in dirs:
+                    newNode = self.getChild(i, j, dir, map)
+                    if newNode not in visited:
+                        queue.append(newNode)
+                        visited.append(newNode)
+        
+        return priorityQueue
+
+    
+
+                
+
+
+
+
